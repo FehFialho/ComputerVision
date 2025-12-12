a5b938f1cc8c02234974d1051cb5e95f6a2947a2
FehFialho <fernandaklechowicz@gmail.com>
2025-12-12 14:17:49 -0300

    farmando aura +100000

---
+++ Keras/corrigir_numpy.ps1
@@ -0,0 +1,4 @@
+./venv/Scripts/activate
+pip uninstall NumPy
+pip install Numpy==1.26.4
+pip install Pillow
--- Kernel/main.py
+++ Kernel/main.py
@@ -1,14 +1,23 @@
 from typing import List
 from Kernel import Kernel, init
 
-def applyKernel(line, column, kernel, image):
+#def getFirstPixel(size):
+
+# print(f"Todos {kernel}\n")
+# print(f"Posição {kernel[0][1]}\n")
+
+def getGrayLevel(i,j, kernel, image):
+    line = i - (len(kernel)) //2
+    column = j - (len(kernel[0])) //2
     s = 0
+
     for lin in range(len(kernel)):
         for col in range(len(kernel[0])):
             s += image[line + lin][column + col] * kernel[lin][col]
 
+    # Normalizações
     sk = sum([sum(k) for k in kernel])
-    if sk <= 0:
+    if sk == 0:
         sk = 1
     s = s / sk
 
@@ -20,30 +29,23 @@ def applyKernel(line, column, kernel, image):
 
     return s
 
-# print(f"Todos {kernel}\n")
-# print(f"Posição {kernel[0][1]}\n")
-
-def getGrayLevel(i,j, kernel, image):
-    line = i - 1
-    column = j - 1
-    return applyKernel(line, column, kernel,image)
-
 def filter_function(image: List[List[int]], kernel: List[List[int]]):
 
     stride = (1,1)
-    filtered = image[:]
+    filtered = []
 
     for i in range(len(image)):
+
+        newLine = []
         for j in range(len(image[i])):
             # borda fica 0
             if (i == 0 or j == 0 or 
                 i == len(image)-1 or 
                 j == len(image[i])-1):
-                filtered[i][j] = 0
+                newLine.append(0)        
             else:
-                filtered[i][j] = getGrayLevel(i, j, kernel, image)
-
-           
+                newLine.append(getGrayLevel(i, j, kernel, image))
+        filtered.append(newLine)
     return filtered
 
 Kernel = Kernel("minion.png", filter_function)
